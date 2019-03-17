f0987034262d0eef6a91ba0f6102e84c1294d8c8
Yash Agarwal <agarwaly899@gmail.com>
2019-03-18 01:35:04 +0530

    skew binary tree

---
+++ Binary Tree/skew tree.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+int count=0;
+typedef struct treenode
+{
+int data;
+struct treenode *left;
+struct treenode *right;
+} binarynode;
+binarynode * temp=NULL;
+binarynode *create()
+{
+binarynode *p;
+int x;
+printf("\nEnter data (-1 for end):");
+scanf("%d",&x);
+if(x== -1)
+return NULL;
+p=(binarynode*)malloc(sizeof(binarynode));
+p->data=x;
+/* to insert left child */
+printf("\nEnter left child of %d:\n",x);
+p->left=create();
+/* To insert right child */
+printf("\nEnter right child of %d:\n",x);
+p->right=create();
+return p; //return the created tree root
+}
+/*int maxdepth(binarynode * node){
+    if(node==NULL){
+        return 0;
+    }
+    int ldepth=maxdepth(node->left);
+    int rdepth=maxdepth(node->right);
+    if(ldepth>rdepth){
+        return(ldepth+1);
+    }
+    else {
+        return (rdepth+1);
+    }
+}*/
+void preorderskew(binarynode * root){
+    if(root!=NULL){
+        if((root->left!=NULL)&&(root->right!=NULL)){
+            printf("\nTree is not skew");
+            exit(0);
+        }
+        preorderskew(root->left);
+        preorderskew(root->right);
+    }
+}
+int main(){ 
+    int key;
+    binarynode *root;
+    root=create();
+    preorderskew(root);
+    printf("\nTree is skew");
+}
