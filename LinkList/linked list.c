f5d5ab7fe18dc033c8e14fcdf45811d731f4f59b
Yash Agarwal <agarwaly899@gmail.com>
2019-07-26 18:11:12 +0530

    some changes in linked list

--- LinkList/linked list.c
+++ LinkList/linked list.c
@@ -53,14 +53,51 @@ void count(){
     }
     printf("\n%d appears %d times!!",key,count);
 }
+void deletenode(){
+    int key;
+    struct node * temp,* prev=start;
+    printf("\nEnter no. to delete node :");
+    scanf("%d",&key);
+    if(start!=NULL){
+        if(start->data==key){
+            temp=start;
+            start=start->link;
+            printf("Done!");
+            return;
+        }
+        else {
+            temp=prev->link;
+            while(temp!=NULL){
+                if(temp->data==key){
+                    prev->link=temp->link;
+                    free(temp);
+                    printf("Done!");
+                    return;
+                }
+                prev=prev->link;
+                temp=temp->link;
+            }
+        }
+    }
+    printf("\nNo such data found!");
+}
+void printlist(struct node * begin){
+    while (begin!=NULL){
+       printf("%d->",begin->data);
+       begin=begin->link;
+    }
+    printf("null");
+}
 int main()
 {   int a;
     while(1){
-        printf("\n1.Enter the node \n2. Search \nEnter option : ");
+        printf("\n1.Enter the node \n2. Search \n3. Delete node\n4. Print list \nEnter option : ");
         scanf("%d",&a);
         switch(a){
             case 1:insertnode();break;
             case 2:count();break;
+		    case 3:deletenode();break;
+		    case 4:printlist(start);break;
             default: exit(0);
         }
     }
