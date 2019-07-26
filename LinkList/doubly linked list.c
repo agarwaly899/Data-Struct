f14d1132a8c47b4dd27d3d2a2c0974efc992e35f
Yash Agarwal <agarwaly899@gmail.com>
2019-07-26 19:03:22 +0530

    some changes in doubly linked list

--- LinkList/doubly linked list.c
+++ LinkList/doubly linked list.c
@@ -74,25 +74,52 @@ void insertbtw(){
     else {
         insertnode();
     }
-    
+    printf("\nDone!");
 }
-void print(){
-    struct node * n;
-    n=start;
-    while(n!=NULL){
-        printf("%d ",n->data);
-        n=n->next;
+void printlist(struct node * begin){
+    while (begin!=NULL){
+       printf("%d->",begin->data);
+       begin=begin->next;
     }
+    printf("null");
+}
+void reverse(struct node * begin){
+    struct node * temp;
+    while (begin!=NULL){
+       temp=begin;
+       begin=begin->next;
+    }
+    while (temp!=NULL){
+       printf("%d->",temp->data); 
+       temp=temp->prev;
+    }
+    printf("null");
+}
+void deletenode(){
+    struct node * temp=start,* hold;
+    int key;
+    printf("\nEnter the element to delete :");
+    scanf("%d",&key);
+    while(temp->data!=key){
+        temp=temp->next;
+    }
+    hold=temp->prev;
+    hold->next=temp->next;
+    hold->next->prev=hold;
+    free(temp);
+    printf("\nDone!");
 }
 int main()
 {   int a;
     while(1){
-        printf("\n1.Enter the node\n2. Print\n3. Insert Between\nEnter option : ");
+        printf("\n1.Enter the node\n2. Print forward\n3. Print reverse\n4.Insert between\n5. Delete node\nEnter option : ");
         scanf("%d",&a);
         switch(a){
             case 1:insertnode();break;
-            case 2:print();break;
-            case 3:insertbtw();break;
+            case 2:printlist(start);break;
+            case 3:reverse(start);break;
+            case 4:insertbtw();break;
+            case 5:deletenode();break;
             default: exit(0);
         }
     }
